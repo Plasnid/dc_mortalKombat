8da1f787393649c52afd04015ea8bce61b6cf81d
Stuart Sackler <stuart.sackler@gmail.com>
2019-03-28 00:34:13 -0400

    upload of all fighters used in the March 27th Mortal Typescript

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Arena from './Arena';
+import FighterA from './FighterA';
+
+import Juju from './Juju';
+import MightyPotato from './MightyPotato';
+
+import GrainboySlim from './grainboy';
+import Carlton from './Carlton';
+
+
+let grain = new GrainboySlim("Grainboy Slim");
+let carl = new Carlton("Carlton");
+let thisRing = new Arena(grain, carl);
